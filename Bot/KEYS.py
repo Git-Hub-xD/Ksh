a10a06fa1d81c6e153da97a487eb32d47b240c9e
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-12-12 10:16:42 +0530

    Update KEYS.py

--- Bot/KEYS.py
+++ Bot/KEYS.py
@@ -1,3 +1,7 @@
 BOT_ADMIN_ID = 6329058409
 
 ALLOWED_GROUPS = [-1002135192853, -1002324159284]
+
+# Game data storage
+current_game = {}  # Stores game data for each chat
+join_timers = {}  # Timers for the join phase
